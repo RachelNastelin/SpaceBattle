530ee518c0df335dbd5192102cd5703e79dbf4d9
bryantcael <bryantca@grinnell.edu>
2018-05-01 15:26:57 -0500

    Working on game board

--- board.c
+++ board.c
@@ -1,39 +1,160 @@
-#include <pthread.h>
-#include <stdbool.h>
+#include "gui.h"
+
+#include <stdint.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <time.h>
 
-#include "board.h"
+#include <SDL.h>
 
-/**
- * Initialize the board display by printing the title and edges
- */
-void init_display(){}
+#define CANNONBALL_RADIUS 5
 
-/**
- * Show a game over message and wait for a key press.
- */
-void end_game(){}
+SDL_Window* sdlwindow;
+SDL_Renderer* sdlrenderer;
+SDL_Texture* sdltexture;
+color_t bitmap[SCREEN_WIDTH * SCREEN_HEIGHT];
 
-/**
- * Run in a thread to draw the current state of the game board.
- */
-void draw_board(){}
+// Initialize the graphical interface
+void gui_init() {
+  // Initialize the SDL library
+  if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+    fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
+    exit(2);
+  }
+  
+  // Create SDL window
+  sdlwindow = SDL_CreateWindow("Galaxy!",
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SCREEN_WIDTH,
+                               SCREEN_HEIGHT,
+                               0);
+  if(sdlwindow == NULL) {
+    fprintf(stderr, "Failed to create SDL window: %s\n", SDL_GetError());
+    SDL_Quit(); // Shut down SDL
+    exit(2);
+  }
+  
+  // Create an SDL renderer
+  sdlrenderer = SDL_CreateRenderer(sdlwindow, -1, 0);
+  if(sdlrenderer == NULL) {
+    fprintf(stderr, "Failed to create SDL renderer: %s\n", SDL_GetError());
+    SDL_DestroyWindow(sdlwindow);
+    SDL_Quit();
+    exit(2);
+  }
+  
+  // Clear the display
+  SDL_SetRenderDrawColor(sdlrenderer, 255, 255, 255, 255);
+  SDL_RenderClear(sdlrenderer);
+  
+  // Create a texture that the renderer will display
+  sdltexture = SDL_CreateTexture(sdlrenderer,
+                                 SDL_PIXELFORMAT_RGBA8888,
+                                 SDL_TEXTUREACCESS_STREAMING,
+                                 SCREEN_WIDTH,
+                                 SCREEN_HEIGHT);
+                               
+  if(sdltexture == NULL) {
+    fprintf(stderr, "Failed to create SDL texture: %s\n", SDL_GetError());
+    SDL_DestroyRenderer(sdlrenderer);
+    SDL_DestroyWindow(sdlwindow);
+    SDL_Quit();
+    exit(2);
+  }
+  
+  // Clear the bitmap the GUI displays
+  memset(bitmap, 0, sizeof(color_t) * SCREEN_WIDTH * SCREEN_HEIGHT);
+}
 
-/**
- * Run in a thread to move the ship around on the board
- */
-void update_ship(){}
+// Update the graphical interface to show the latest image data
+void gui_update_display() {
+  // Get the raw texture data from SDL
+  uint32_t* texture_data;
+  int pitch;
+  SDL_LockTexture(sdltexture, NULL, (void**)&texture_data, &pitch);
 
-/**
- * Run in a thread to generate cannonballs on the board.
- */
-void generate_cannonball(){}
+  // Copy in our bitmap data
+  memcpy(texture_data, bitmap, sizeof(color_t) * SCREEN_WIDTH * SCREEN_HEIGHT);
+
+  // Release the raw texture data
+  SDL_UnlockTexture(sdltexture);
+  
+  // Copy the texture to the renderer
+  SDL_Rect destination = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+  SDL_RenderCopy(sdlrenderer, sdltexture, NULL, &destination);
+  
+  // Display the bitmap
+  SDL_RenderPresent(sdlrenderer);
+}
+
+// Set a single pixel in the image data
+void gui_set_pixel(int x, int y, color_t color) {
+  if(x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT) {
+    bitmap[y * SCREEN_WIDTH + x] = color;
+  }
+}
+
+// Add a circle to the image data
+void gui_draw_circle(int center_x, int center_y, float radius, color_t color) {
+  // Loop over one quadrant of the circle's points
+  // Based on method from http://groups.csail.mit.edu/graphics/classes/6.837/F98/Lecture6/circle.html
+  for(int x=0; x<=radius; x++) {
+    for(int y=0; y<=radius; y++) {
+      // If this point is inside the circle, draw a point in all four quadrants
+      if(x * x + y * y < radius * radius) {
+        gui_set_pixel(center_x + x, center_y + y, color);
+        gui_set_pixel(center_x + x, center_y - y, color);
+        gui_set_pixel(center_x - x, center_y + y, color);
+        gui_set_pixel(center_x - x, center_y - y, color);
+      }
+    }
+  }
+}
+
+//Drawing ship (it's a square)
+void gui_draw_ship(int center_x, int center_y){
+  // Based on method from http://groups.csail.mit.edu/graphics/classes/6.837/F98/Lecture6/circle.html
+  for(int x=0; x<=radius; x++) {
+    for(int y=0; y<=radius; y++) {
+      gui_set_pixel(center_x + x, center_y + y, color);
+      gui_set_pixel(center_x + x, center_y - y, color);
+      gui_set_pixel(center_x - x, center_y + y, color);
+      gui_set_pixel(center_x - x, center_y - y, color);
+    }
+  }
+}
+
+
+//Drawing cannonballs
+void gui_draw_cannonballs(int center_x, int center_y){
+  // Loop over one quadrant of the circle's points
+  // Based on method from http://groups.csail.mit.edu/graphics/classes/6.837/F98/Lecture6/circle.html
+  for(int x=0; x<=CANNONBALL_RADIUS; x++) {
+    for(int y=0; y<=CANNONBALL_RADIUS; y++) {
+      // If this point is inside the circle, draw a point in all four quadrants
+      if(x * x + y * y < CANNONBALL_RADIUS * CANNONBALL_RADIUS) {
+        gui_set_pixel(center_x + x, center_y + y, color);
+        gui_set_pixel(center_x + x, center_y - y, color);
+        gui_set_pixel(center_x - x, center_y + y, color);
+        gui_set_pixel(center_x - x, center_y - y, color);
+      }
+    }
+  }
+}
+// Fade out every pixel by some scale
+void gui_fade(float scale) {
+  for(int i=0; i<SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+    bitmap[i].red *= scale;
+    bitmap[i].green *= scale;
+    bitmap[i].blue *= scale;
+  }
+}
+
+// Clean up
+void gui_shutdown() {
+  // Clean up before exiting
+  SDL_DestroyTexture(sdltexture);
+  SDL_DestroyRenderer(sdlrenderer);
+  SDL_DestroyWindow(sdlwindow);
+  SDL_Quit();
+}

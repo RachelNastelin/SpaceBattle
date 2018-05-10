9e85aaeb32e690db15e63909526ee8616c9f084a
Rachel Nastelin <nastelin@cs.grinnell.edu>
2018-05-09 21:11:51 -0500

    Updates

--- board.h
+++ board.h
@@ -1,4 +1,3 @@
-#include "gui.h"
 
 #include <stdint.h>
 #include <stdio.h>
@@ -7,8 +6,29 @@
 #include <SDL.h>
 
 #ifndef __BOARD_H__
-#define __BOARD_H__
+#define __BOARD_H__		
 
+/** From gui.h **/
+ // Screen size		
+#define SCREEN_WIDTH 1024		
+#define SCREEN_HEIGHT 768		
+		
+typedef struct color {		
+  uint8_t alpha;		
+  uint8_t blue;		
+  uint8_t green;		
+  uint8_t red;		
+} color_t;		
+		
+void gui_init();		
+void gui_update_display();		
+void gui_set_pixel(int x, int y, color_t color);		
+void gui_draw_circle(int center_x, int center_y, float radius,
+                     color_t color);		
+void gui_fade(float scale);		
+void gui_shutdown();		
+
+/** From board.h  **/
 // Initialize the graphical interface
 void gui_init();
 
